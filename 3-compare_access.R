9adba5409f60db26519c551fed7cda21eabd5402
Kaue Braga <kauebraga90@gmail.com>
2021-08-10 22:28:29 -0300

    update

--- 3-compare_access.R
+++ 3-compare_access.R
@@ -6,7 +6,17 @@ library(readr)
 library(ggplot2)
 library(hrbrthemes)
 library(mapview)
+library(patchwork)
+library(scales)
 options(scipen = 999)
+
+
+ks <- function (x) { scales::label_number(accuracy = 1,
+                                          scale = 1/1000,
+                                          suffix = "k",
+                                          big.mark = ",")(x)
+  
+}
 
 
 # atencai aqyu
@@ -65,158 +75,74 @@ acess_dif_wide <- acess %>%
 limits_ind <- acess_dif_wide %>%
   st_set_geometry(NULL) %>%
   group_by(ind) %>%
-  summarise(dif_abs = abs(min(dif_abs, na.rm = TRUE)),
-            dif_log_tc = abs(min(dif_log_tc, na.rm = TRUE))) %>% setDT()
+  summarise(dif_abs = max(abs(dif_abs), na.rm = TRUE),
+            dif_log_tc = max(abs(dif_log_tc), na.rm = TRUE)) %>% setDT()
 
 
-# var <- "CMATT60"
+# var <- "CMATT60"; tipo <- "dif_abs
 
 # function to maps
 fazer_mapa <- function(var, tipo) {
   
   # get limits
-  limits_scale <- limits_ind[ind == var] %>% pull({{tipo}})
-    
-    acess_dif_wide %>%
+  limits_scale <- limits_ind[ind == var] %>% pull(!!rlang::sym(tipo))
+  # print(limits_scale)
+  # limits_ind[ind == var] %>% pull{tipo)
+  
+  labelss <- if (grepl("CMA", var) & tipo == "dif_abs") ks else label_number(accuracy = 0.01)
+  
+  
+  acess_dif_wide %>%
     # filter(stringr::str_detect(ind, "TT")) %>%
     filter(stringr::str_detect(ind, var)) %>%
     ggplot()+
-    geom_sf(aes(fill = {{tipo}}), color = NA)+
+    geom_sf(aes(fill = !!rlang::sym(tipo)), color = NA)+
     geom_sf(data = limits, fill = NA)+
     scale_fill_distiller(palette = "RdBu", direction = 1,
-                         limits = c(-1,1)*limits_scale
+                         limits = c(-1,1)*limits_scale,
                          # breaks = c(-0.5, 0, 0.5),
-                         # labels = c("-50%", "0", "+50%"))+
+                         labels = labelss
     ) +
+    labs(
+      fill = ""
+      # title = var
+      )+
     theme_mapa()
 }
 
-
-a <- lapply(c("CMATT45", "FCATT45",
-              "CMATT60", "FCATT60",
-              "CMATT90", "FCATT90"),
-            fazer_mapa, tipo = dif_abs)
+# apply function
+a <- lapply(c(
+  # "CMATT45", "FCATT45",
+  "CMATT60", "FCATT60",
+  "CMATT90", "FCATT90"),
+  fazer_mapa, tipo = "dif_abs")
 
 maps_abs <- purrr::reduce(a, `+`) + plot_layout(ncol = 2)
 
-b <- lapply(c("CMATT45", "FCATT45",
-              "CMATT60", "FCATT60",
-              "CMATT90", "FCATT90"),
-            fazer_mapa, tipo = dif_log_tc)
+ggsave(filename = "figures/anpet_2021/map_comp_abs_PRP50.png",
+       plot = maps_abs,
+       units = "cm",
+       width = 16,
+       height = 14)
+
+b <- lapply(c(
+  # "CMATT45", "FCATT45",
+  "CMATT60", "FCATT60",
+  "CMATT90", "FCATT90"),
+  fazer_mapa, tipo = "dif_log_tc")
 
 maps_log <- purrr::reduce(b, `+`) + plot_layout(ncol = 2)
 
-library(patchwork)
-# boxplot.stats(for_comparacao_tt$dif_log)$stats[2:4]
-# comparacao espacial
-map_difabs_TT_CMA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "CMATT60")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_abs), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_cma_abs)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença de oportunidades\n acessíveis")+
-  theme_mapa()
+ggsave(filename = "figures/anpet_2021/map_comp_log_PRP50.png",
+       plot = maps_log,
+       units = "cm",
+       width = 16,
+       height = 14)
 
-
-
-map_difabs_TT_FCA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "FCATT60")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_abs), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_tsfca_abs)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença de oportunidades\n acessíveis")+
-  theme_mapa()
-
-map_difabs_TT_CMA + map_difabs_TT_FCA
 
 # mapview breaks
 # https://stackoverflow.com/questions/55485100/mapview-legend-scaling
 # mapview(acess_dif_wide, zcol = "dif_abs", col.regions = RColorBrewer::brewer.pal(10, "RdBu"), col = NULL)
-
-
-
-
-# diferenca relativa
-
-map_difrel_TT_CMA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "CMATT60")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_log_tc), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_cma_rel)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença relativa de oportunidades\n acessíveis")+
-  theme_mapa()
-
-
-
-map_difrel_TT_FCA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "TSFCATT")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_log_tc), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_tsfca_rel)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença de oportunidades\n acessíveis")+
-  theme_mapa()
-
-map_difrel_TT_CMA + map_difrel_TT_FCA
-
-# diference between the cma and tsfca
-dif_CMA_TSFCA <- acess_dif_wide %>%
-  select(origin, ind, dif_log_tc) %>%
-  filter(stringr::str_detect(ind, "CMATT|TSFCATT")) %>%
-  spread(key = ind,
-         value = dif_log_tc,
-         sep = "_"
-  ) %>%
-  mutate(dif = abs(ind_TSFCATT65  - ind_CMATT65)) %>%
-  mutate(dif_tc = ifelse(dif > 0.2, 0.2, dif))
-
-boxplot(dif_CMA_TSFCA$dif)
-mapview(dif_CMA_TSFCA, zcol = "dif_tc")
-
-mean(dif_CMA_TSFCA$ind_CMATT65, na.rm = TRUE)
-mean(dif_CMA_TSFCA$ind_TSFCATT65, na.rm = TRUE)
-
-acess_dif_wide %>%
-  filter(stringr::str_detect(ind, "CMATT|TSFCATT")) %>%
-  ggplot()+
-  geom_boxplot(aes(x = ind, y = dif_log))
-
-ggplot()+
-  geom_sf(data = dif_CMA_TSFCA, aes(fill = dif_tc), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_viridis_c(option = "inferno")+
-  labs(fill = "Vermelho: A dif do CMA eh maior")+
-  theme_mapa()
-
-
 
 
 
@@ -238,142 +164,82 @@ acess_dif_wide <- acess %>%
   filter(!is.na(dif_abs)) %>%
   filter(!is.infinite(dif_log)) %>%
   filter(!is.na(dif_log)) %>%
-  mutate(dif_log_tc = ifelse(dif_log > 0.5, 0.5,
-                             ifelse(dif_log < -0.5, -0.5, dif_log))) %>%
+  mutate(dif_log_tc = ifelse(dif_log > 0.8, 0.8,
+                             ifelse(dif_log < -0.8, -0.8, dif_log))) %>%
   st_sf(crs = 4326)
 
 
+
+# acess_dif_wide %>% filter(ind == "FCATT45") %>% filter(dif_abs > 0.126)
 
 # library(mapview)
 # a <- acess_dif_wide %>% filter(stringr::str_detect(ind, "CMATT"))
 # mapview(a, zcol = "dif_abs", col.regions = RColorBrewer::brewer.pal(10, "RdBu"), col = NULL)
 
 # limits for each indicator
-limits_cma_abs <- acess_dif_wide %>%
-  filter(stringr::str_detect(ind, "CMATT")) %>%
-  pull(dif_abs) %>% abs() %>% max()
-
-limits_tsfca_abs <- acess_dif_wide %>%
-  filter(stringr::str_detect(ind, "^FCATT")) %>%
-  pull(dif_abs) %>% abs() %>% max() 
-
-limits_cma_rel <- acess_dif_wide %>%
-  filter(stringr::str_detect(ind, "CMATT")) %>%
-  pull(dif_log_tc) %>% abs() %>% max()
-
-limits_tsfca_rel <- acess_dif_wide %>%
-  filter(stringr::str_detect(ind, "^FCATT")) %>%
-  pull(dif_log_tc) %>% abs() %>% max()
+limits_ind <- acess_dif_wide %>%
+  st_set_geometry(NULL) %>%
+  group_by(ind) %>%
+  summarise(dif_abs = max(abs(dif_abs), na.rm = TRUE),
+            dif_log_tc = max(abs(dif_log_tc), na.rm = TRUE)) %>% setDT()
 
 
+# var <- "CMATT60"; tipo <- "dif_abs
 
-library(patchwork)
-# boxplot.stats(for_comparacao_tt$dif_log)$stats[2:4]
-# comparacao espacial
-map_difabs_TT_CMA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "CMATT60")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_abs), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "OrRd", direction = -1)+
-  labs(fill = "Diferença de oportunidades\n acessíveis")+
-  theme_mapa()
+# function to maps
+fazer_mapa <- function(var, tipo) {
+  
+  # get limits
+  limits_scale <- limits_ind[ind == var] %>% pull(!!rlang::sym(tipo))
+  # print(limits_scale)
+  # limits_ind[ind == var] %>% pull{tipo)
+  
+  labelss <- if (grepl("CMA", var) & tipo == "dif_abs") ks else label_number(accuracy = 0.01)
+  
+  
+  acess_dif_wide %>%
+    # filter(stringr::str_detect(ind, "TT")) %>%
+    filter(stringr::str_detect(ind, var)) %>%
+    ggplot()+
+    geom_sf(aes(fill = !!rlang::sym(tipo)), color = NA)+
+    geom_sf(data = limits, fill = NA)+
+    scale_fill_distiller(palette = "RdBu", direction = 1,
+                         limits = c(-1,1)*limits_scale,
+                         # breaks = c(-0.5, 0, 0.5),
+                         labels = labelss
+    ) +
+    labs(
+      fill = ""
+      # title = var
+    )+
+    theme_mapa()
+}
 
-summary(subset(acess_dif_wide, ind == "CMATT65")$dif_abs)
-summary(subset(acess_dif_wide, ind == "FCATT65")$dif_abs)
+# apply function
+a <- lapply(c(
+  # "CMATT45", "FCATT45",
+  "CMATT60", "FCATT60",
+  "CMATT90", "FCATT90"),
+  fazer_mapa, tipo = "dif_abs")
 
-map_difabs_TT_FCA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "^FCATT45")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_abs), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  # scale_fill_distiller(palette = "RdBu", direction = 1)+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_tsfca_abs)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença de oportunidades\n acessíveis")+
-  theme_mapa()
+maps_abs <- purrr::reduce(a, `+`) + plot_layout(ncol = 2)
 
-map_difabs_TT_CMA + map_difabs_TT_FCA
+ggsave(filename = "figures/anpet_2021/map_comp_abs_P50P85.png",
+       plot = maps_abs,
+       units = "cm",
+       width = 16,
+       height = 14)
 
-# mapview breaks
-# https://stackoverflow.com/questions/55485100/mapview-legend-scaling
-# mapview(acess_dif_wide, zcol = "dif_abs", col.regions = RColorBrewer::brewer.pal(10, "RdBu"), col = NULL)
+b <- lapply(c(
+  # "CMATT45", "FCATT45",
+  "CMATT60", "FCATT60",
+  "CMATT90", "FCATT90"),
+  fazer_mapa, tipo = "dif_log_tc")
 
-# diferenca relativa -----------------------------
+maps_log <- purrr::reduce(b, `+`) + plot_layout(ncol = 2)
 
-map_difrel_TT_CMA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "CMATT")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_log_tc), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_cma_rel)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença relativa de oportunidades\n acessíveis")+
-  theme_mapa()
-
-
-
-map_difrel_TT_FCA <- acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "^FCATT")) %>%
-  ggplot()+
-  geom_sf(aes(fill = dif_log), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_distiller(palette = "RdBu", direction = 1,
-                       limits = c(-1,1)*max(limits_tsfca_rel)
-                       # breaks = c(-0.5, 0, 0.5),
-                       # labels = c("-50%", "0", "+50%")
-  )+
-  labs(fill = "Diferença de oportunidades\n acessíveis")+
-  theme_mapa()
-
-
-acess_dif_wide %>%
-  # filter(stringr::str_detect(ind, "TT")) %>%
-  filter(stringr::str_detect(ind, "^FCATT")) %>%
-  mapview(zcol = "dif_log_tc", col.regions = RColorBrewer::brewer.pal(10, "RdBu"), col = NULL)
-
-map_difrel_TT_CMA + map_difrel_TT_FCA
-
-# diference between the cma and tsfca
-dif_CMA_TSFCA <- acess_dif_wide %>%
-  select(origin, ind, dif_log_tc) %>%
-  filter(stringr::str_detect(ind, "CMATT|TSFCATT")) %>%
-  spread(key = ind,
-         value = dif_log_tc,
-         sep = "_"
-  ) %>%
-  mutate(dif = abs(ind_TSFCATT65  - ind_CMATT65)) %>%
-  mutate(dif_tc = ifelse(dif > 0.2, 0.2, dif))
-
-boxplot(dif_CMA_TSFCA$dif)
-mapview(dif_CMA_TSFCA, zcol = "dif_tc")
-
-mean(dif_CMA_TSFCA$ind_CMATT65, na.rm = TRUE)
-mean(dif_CMA_TSFCA$ind_TSFCATT65, na.rm = TRUE)
-
-acess_dif_wide %>%
-  filter(stringr::str_detect(ind, "CMATT|TSFCATT")) %>%
-  ggplot()+
-  geom_boxplot(aes(x = ind, y = dif_log))
-
-ggplot()+
-  geom_sf(data = dif_CMA_TSFCA, aes(fill = dif_tc), color = NA)+
-  geom_sf(data = limits, fill = NA)+
-  # scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0, )+
-  scale_fill_viridis_c(option = "inferno")+
-  labs(fill = "Vermelho: A dif do CMA eh maior")+
-  theme_mapa()
+ggsave(filename = "figures/anpet_2021/map_comp_log_P50P85.png",
+       plot = maps_log,
+       units = "cm",
+       width = 16,
+       height = 14)
